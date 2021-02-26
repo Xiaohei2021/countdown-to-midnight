abd963e71d12b1e65b8d4022521b1e192a0e5132
Leon <hualiang.tian@gmail.com>
2021-02-25 20:28:57 -0500

    commit tree 12a3d7f323e4bffbba97ea4828edeb93ba4cabde

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
 
-def countdown
+#write your code here
+x = 5 
+def countdown(x)
+   until x === 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1
+   end
+    return "HAPPY NEW YEAR!"
+end
+
+x=5
+def countdown_with_sleep(x)
+    until x === 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep (1)
+    end
 end
